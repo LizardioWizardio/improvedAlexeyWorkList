6071c40c95711483bddc459cce7153f5b6eb2a80
AlexeyPipchuk <alexeypipchuk@gmail.com>
2019-03-22 05:44:24 +0700

    Frontend

--- app/src/main/java/h/alexeypipchuk/worklist/MainActivity.java
+++ app/src/main/java/h/alexeypipchuk/worklist/MainActivity.java
@@ -1,7 +1,10 @@
 package h.alexeypipchuk.worklist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Заполнение меню; элементы (если они есть) добавляются на панель действий.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_new_plan:
+                Intent intent = new Intent(this, Plan.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ app/src/main/java/h/alexeypipchuk/worklist/Plan.java
@@ -0,0 +1,59 @@
+package h.alexeypipchuk.worklist;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class Plan extends AppCompatActivity {
+
+    EditText Caption;
+    EditText Date;
+    EditText Description;
+    RadioGroup StatusGroup;
+    RadioGroup ImportantGroup;
+    FloatingActionButton btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan);
+
+        Caption = (EditText) findViewById(R.id.caption);
+        Date = (EditText) findViewById(R.id.date);
+        Description = (EditText) findViewById(R.id.description);
+        StatusGroup = (RadioGroup) findViewById(R.id.StatusGroup);
+        ImportantGroup = (RadioGroup) findViewById(R.id.ImportantGroup);
+        btn = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+
+                if(Caption.getText() == null || StatusGroup.getCheckedRadioButtonId() == -1 || ImportantGroup.getCheckedRadioButtonId() == -1)
+                {
+                    Toast.makeText(getApplicationContext(), "Заполните обязательные поля", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    View StatusView = StatusGroup.findViewById(StatusGroup.getCheckedRadioButtonId());
+                    RadioButton SattusRb = (RadioButton)StatusGroup.getChildAt(StatusGroup.indexOfChild(StatusView));
+                    String StatusState = SattusRb.getText().toString();
+
+                    View ImportantView = ImportantGroup.findViewById(ImportantGroup.getCheckedRadioButtonId());
+                    RadioButton ImportantRb = (RadioButton)ImportantGroup.getChildAt(ImportantGroup.indexOfChild(ImportantView));
+                    String ImportantState = ImportantRb.getText().toString();
+
+                    startActivity(new Intent(Plan.this, MainActivity.class));
+                }
+            }
+        });
+    }
+}
