ca36decf799a27119a83b6847d6b5eb618420993
AlexeyPipchuk <alexeypipchuk@gmail.com>
2019-03-22 15:44:56 +0700

    think about mvvm

---
+++ app/src/main/java/h/alexeypipchuk/worklist/Note.java
@@ -0,0 +1,49 @@
+package h.alexeypipchuk.worklist;
+
+public class Note {
+    private String mCaption;
+    private String mStatus;
+    private String mDescription;
+    private String mDate;
+    private String mImportance;
+
+    public String getmCaption() {
+        return mCaption;
+    }
+
+    public void setmCaption(String mCaption) {
+        this.mCaption = mCaption;
+    }
+
+    public String getmStatus() {
+        return mStatus;
+    }
+
+    public void setmStatus(String mStatus) {
+        this.mStatus = mStatus;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public void setmDescription(String mDescription) {
+        this.mDescription = mDescription;
+    }
+
+    public String getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(String mDate) {
+        this.mDate = mDate;
+    }
+
+    public String getmImportance() {
+        return mImportance;
+    }
+
+    public void setmImportance(String mImportance) {
+        this.mImportance = mImportance;
+    }
+}
---
+++ app/src/main/java/h/alexeypipchuk/worklist/ViewModel.java
@@ -0,0 +1,8 @@
+package h.alexeypipchuk.worklist;
+
+public class ViewModel extends android.arch.lifecycle.ViewModel {
+
+    public ViewModel(Note note){
+
+    }
+}
